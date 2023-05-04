b5caa850d56eb9792eedf630e7d65253879ab00f
StefanVIP <steptarget@yandex.ru>
2023-05-04 17:02:51 +0300

    Make corrections in lesson7

--- Lesson7/templates/task_2_welcome_message.php
+++ Lesson7/templates/task_2_welcome_message.php
@@ -1,3 +1,4 @@
+<?php global $userName?>
 <section class="space-y-8">
     <p class="text-3xl font-bold">Добро пожаловать</p>
     <p><b><?php echo $userName ?></b>, привет. Мы рады тебя приветствовать на нашем сайте!</p>
