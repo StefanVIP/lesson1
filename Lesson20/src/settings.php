2751c6cc04f592d7e99b442c4042c5fc0ac6afef
Alexey Boychenko <aboychenko@xiag.ch>
2023-06-16 23:02:28 +0300

    Fix db config

--- Lesson20/src/settings.php
+++ Lesson20/src/settings.php
@@ -3,6 +3,6 @@
 return [
     'host' => '127.0.0.1', // Адрес сервера
     'dbname' => 'lessondb', // Название БД
-    'user' => 'userdev1', // Имя пользователя
-    'password' => 'password4dev1', // Пароль пользователя
+    'user' => 'dbuser', // Имя пользователя
+    'password' => 'pass', // Пароль пользователя
 ];
