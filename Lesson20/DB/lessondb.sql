73a562890da307e3d310d12b5344fe75e433348f
Alexey Boychenko <aboychenko@xiag.ch>
2023-06-16 22:22:49 +0300

    Fixed db dump

--- Lesson20/DB/lessondb.sql
+++ Lesson20/DB/lessondb.sql
@@ -16,33 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `group_user`
---
-
-DROP TABLE IF EXISTS `group_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `group_user` (
-  `group_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  UNIQUE KEY `user_group_index` (`group_id`,`user_id`),
-  KEY `user_id` (`user_id`) /*!80000 INVISIBLE */,
-  CONSTRAINT `group_user_1` FOREIGN KEY (`group_id`) REFERENCES `u_groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `group_user_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `group_user`
---
-
-LOCK TABLES `group_user` WRITE;
-/*!40000 ALTER TABLE `group_user` DISABLE KEYS */;
-INSERT INTO `group_user` VALUES (3,1),(3,2),(3,3),(3,4),(4,1),(4,3),(4,4);
-/*!40000 ALTER TABLE `group_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `u_groups`
 --
 
@@ -97,6 +70,37 @@ LOCK TABLES `users` WRITE;
 INSERT INTO `users` VALUES (1,'Степан','Петрович','Ходько','khodko@site.ru','+79881231211','$2y$10$4NglPjX.0O6XphHKY6YNXeMhvibpLguzHWxamI7YFWfwSv7V0v8fW',0,1),(2,'Игорь','Иванович','Аникеев','anikeev@site.ru','+79881231212','$2y$10$4NglPjX.0O6XphHKY6YNXeMhvibpLguzHWxamI7YFWfwSv7V0v8fW',0,1),(3,'Артем','Амазаспович','Абрамян','abramian@site.ru','+79881231213','$2y$10$4NglPjX.0O6XphHKY6YNXeMhvibpLguzHWxamI7YFWfwSv7V0v8fW',0,0),(4,'Владислав','Граф','Дракула','drakula@site.ru','+79881231214','$2y$10$4NglPjX.0O6XphHKY6YNXeMhvibpLguzHWxamI7YFWfwSv7V0v8fW',0,0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+-- Dump completed on 2023-06-16 15:22:17
+
+    --
+-- Table structure for table `group_user`
+--
+
+DROP TABLE IF EXISTS `group_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `group_user` (
+                              `group_id` int NOT NULL,
+                              `user_id` int NOT NULL,
+                              UNIQUE KEY `user_group_index` (`group_id`,`user_id`),
+    KEY `user_id` (`user_id`) /*!80000 INVISIBLE */,
+    CONSTRAINT `group_user_1` FOREIGN KEY (`group_id`) REFERENCES `u_groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `group_user_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `group_user`
+--
+
+LOCK TABLES `group_user` WRITE;
+/*!40000 ALTER TABLE `group_user` DISABLE KEYS */;
+INSERT INTO `group_user` VALUES (3,1),(3,2),(3,3),(3,4),(4,1),(4,3),(4,4);
+/*!40000 ALTER TABLE `group_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -106,5 +110,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-06-16 15:22:17
